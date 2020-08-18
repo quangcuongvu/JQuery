c03ac2aa255fb35a5e5968344ddfe0f1756397dd
QuangCuong <cophongsuongbn@gmail.com>
2020-08-19 03:00:54 +0700

    up file đến bài số 23

--- Bai17/1.js
+++ Bai17/1.js
@@ -1,3 +1,42 @@
 $(document).ready(function () {
     console.log("hello");
+    // xu ly viec click thay doi mau
+    $('.tdkhoi').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('xanh');
+
+        // khi click vao, cho noi dung hien ra
+        $(this).next().slideToggle(500);
+        $('html, body').animate({scrollTop: $(this).offset().top});
+    });
+    //code cho phan phong to anh
+    $(".vaicaianh a").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'none',
+
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        closeBtn  : false,
+
+        helpers : {
+            title : {
+                type : 'inside'
+            },
+            buttons	: {},
+            thumbs : {
+                width  : 0,
+                height : 0,
+            }
+        },
+
+        afterLoad : function() {
+            this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
+        }
+    });
+
+    // xu ly co dan noi dung accordion
+    $('.ndkhoi').slideUp();
+    
+
 });
